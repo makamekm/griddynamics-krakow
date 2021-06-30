1c7ac8407a5f5a67d9b0cb683f3cda4d6e6fc5f1
Makame <makame@mail.ru>
2021-06-30 13:28:06 +0200

    Changes

---
+++ src/pages/index.js
@@ -0,0 +1,151 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Image, Text, Box, Section } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { GlobalQuarklyPageStyles } from "global-page-styles";
+import { RawHtml, Override, Menu, StackItem, Stack } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<GlobalQuarklyPageStyles pageUrl={"index"} />
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section>
+			<Box
+				display="flex"
+				padding="12px 0"
+				justify-content="space-between"
+				align-items="center"
+				flex-direction="row"
+				md-flex-direction="column"
+			>
+				<Box flex="1 1 0%" display="flex" justify-content="flex-start" align-items="flex-start">
+					<Image width="170px" height="40px" src="https://gist.githubusercontent.com/makamekm/6d0591ff95ee94f204210d35dedc44f9/raw/3818e305c382b58ec657f49c0a8ec151a4e4408d/griddynamics.svg" />
+					<Text margin="16px 0px 16px 20px" flex="0 1 auto" font="normal 600 20px/80% --fontFamily-googleNunito" color="rgba(0, 0, 0, 0.5)">
+						|
+					</Text>
+					<Text margin="16px 0px 16px 15px" flex="0 1 auto" font="normal 600 18px/100% --fontFamily-googleNunito" color="rgba(0, 0, 0, 0.8)">
+						Krakow
+					</Text>
+				</Box>
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+				>
+					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px" />
+					<Override slot="link-index">
+						Welcome
+					</Override>
+				</Menu>
+				<Text margin="16px 0px 16px 0px" flex="0 1 auto" font="normal 600 20px/80% --fontFamily-googleNunito" color="rgba(0, 0, 0, 0.4)">
+					|
+				</Text>
+				<Menu
+					display="flex"
+					justify-content="center"
+					font="--base"
+					font-weight="700"
+					md-flex-direction="column"
+					md-align-items="center"
+				>
+					<Override slot="link" text-decoration="none" color="--dark" padding="6px 12px" />
+					<Override slot="link-active" color="--primary" />
+					<Override slot="item" padding="6px" />
+					<Override slot="link-index">
+						Links
+					</Override>
+				</Menu>
+			</Box>
+		</Section>
+		<Section
+			padding="100px 0 100px 0"
+			sm-padding="40px 0"
+			background="linear-gradient(0deg,rgba(0,0,0,.4) 0%,rgba(0,0,0,.8) 100%),--color-dark url(https://images.unsplash.com/photo-1495181941197-70a606546974?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2812&q=80) 50% 15%/cover"
+			color="--light"
+			font="--base"
+		>
+			<Box>
+				<Text
+					as="h1"
+					margin="0 0 96px 0"
+					text-align="center"
+					letter-spacing="50px"
+					text-transform="uppercase"
+					font="100 132px/1.2 --fontFamily-sans"
+					lg-font="200 42px/1.2 --fontFamily-sans"
+					lg-letter-spacing="20px"
+				>
+					GRID DYNAMICS
+					<br />
+					Poland
+				</Text>
+			</Box>
+			<Stack>
+				<StackItem
+					width="25%"
+					lg-width="50%"
+					sm-width="100%"
+					font="--lead"
+					text-transform="uppercase"
+					letter-spacing="1px"
+				>
+					<Text margin="0px">
+						About me
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						My name is Mary Cornell, born and raised in France.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						I've been working as a professional photographer and videographer for more than 12 years.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						CDS Documentary Essay Prize and Best Photographer of The Year 2019 by Sony World Photography Awards.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+		<RawHtml>
+			<style place={"endOfHead"} rawKey={"60dc4f023f4eb1001ea278ee"}>
+				{":root {\n  box-sizing: border-box;\n}\n\n* {\n  box-sizing: inherit;\n}"}
+			</style>
+		</RawHtml>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,29 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"60dc4f023f4eb1001ea278f3",
+				"60dc4f023f4eb1001ea278f5"
+			]
+		},
+		"60dc4f023f4eb1001ea278f3": {
+			"id": "60dc4f023f4eb1001ea278f3",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"60dc4f023f4eb1001ea278f5": {
+			"id": "60dc4f023f4eb1001ea278f5",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app",
+	"site": {
+		"styles": {},
+		"seo": {}
+	}
+}
